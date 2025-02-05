ab04d0cd22ef03ea7de507189eab6a4e28065859
DIVINEMICHAEL04 <divinegodwinmichael@gmail.com>
2025-02-05 21:06:20 +0100

    hello

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const para = document.querySelector('[data-testid = "shortBio"]');
-para.innerText
-console.log(para.innerText);
+const profilePicture = document.querySelector('[data-testid="profilePicture"]');
+profilePicture.innerText
 
-const date = new Date();
-document.querySelector(".time").innerHTML = date.toISOString();
+const fullName = document.querySelector('[data-testid="fullName"]');
+fullName.innerText
+
+const jobTitle = document.querySelector('[data-testid="jobTitle"]');
+jobTitle.innerText
+
+const shortBio = document.querySelector('[data-testid="shortBio"]');
+shortBio.innerText
+
+const emailAddress = document.querySelector('[data-testid="emailAddress"]');
+emailAddress.innerText
+
+const socialLinks = document.querySelector('[data-testid="my-socials"]');
+socialLinks.innerText
+
+const currentTimeUTC = document.querySelector('[data-testid="currentTimeUTC"]');
+currentTimeUTC.innerText
+
+const timeElement = document.querySelector(".time");
+timeElement.innerHTML = new Date().toISOString();
